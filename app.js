ae149e8dd68dec81199aeda20f0a57801fc53242
Raja784 <mandavaraja95@gmail.com>
2023-04-07 11:26:32 -0500

    changes has been done

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var sugarRouter = require('./routes/sugar');
 var boardRouter = require('./routes/board');
-var selenpmctorRouter = require('./routes/selector');
+var selectorRouter = require('./routes/selector');
 
 
 var app = express();
